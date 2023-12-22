692e8d524f34b3bd465e0d6ca83326de49558d90
Nandrasantsoa Stephan Gabriel <stephangabrielnandrasantsoa@gmail.com>
2023-12-22 09:43:37 +0100

    modifiy util.js

--- js/util.js
+++ js/util.js
@@ -1,4 +1,33 @@
+let inputBean = {
+  firstName: '',
+  lastName: '',
+  email: '',
+  phone: '',
+  psw: '',
+}
+
 const handleSubmit = (event) => {
   event.preventDefault()
   console.log('submit')
+  const inputs = document.getElementsByTagName('input')
+  // for (let index = 0; index < inputs.length - 1; index++) {
+  //   console.log(inputs[index].checkValidity())
+  // }
 }
+const handleChangeInput = ({ target }) => {
+  console.log(target.id, target.value)
+  inputBean[target.id] = target.value
+}
+
+document.getElementById('confirmPsw').addEventListener('input', (event) => {
+  const p = document.getElementById('confirmPswError')
+  alert('change')
+  if (inputBean.psw !== event.target.value) {
+    p.style.visibility = ''
+    p.innerText = 'different from password.'
+  } else {
+    p.innerText = ''
+    p.style.visibility = 'hidden'
+  }
+  console.log('changeConfirmPsw')
+})
